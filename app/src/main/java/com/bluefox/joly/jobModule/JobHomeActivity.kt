395830211a683cbf7fdd35caef3a451e8e09f6ab
rajeshbluefox <bluefoxmobileapps@gmail.com>
2024-07-16 15:23:28 +0530

    Job Provider Screen 3

--- app/src/main/java/com/bluefox/joly/jobModule/JobHomeActivity.kt
+++ app/src/main/java/com/bluefox/joly/jobModule/JobHomeActivity.kt
@@ -23,7 +23,6 @@ class JobHomeActivity : AppCompatActivity() {
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var loginAPIFunctions: LoginAPIFunctions
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityJobHomeBinding.inflate(layoutInflater)
